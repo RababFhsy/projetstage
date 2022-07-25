2d15de4b9d3e0ab246b2da812aa963986d8965f3
Rabab Fahssi <fhsyrabab07@gmail.com>
2022-07-25 17:12:51 +0100

    third commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gestion_cab');
+define('DB_SERVER', 'sql8.freesqldatabase.com');
+define('DB_USERNAME', 'sql8508680');
+define('DB_PASSWORD', 'HcAvki9g6u');
+define('DB_NAME', 'sql8508680');
  
 // Connexion � la base de donn�es MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-	$db_username = 'root';
-	$db_password = '';
-	$conn = new PDO( 'mysql:host=localhost;dbname=gestion_cab', $db_username, $db_password );
+	$db_username = 'sql8508680';
+	$db_password = 'HcAvki9g6u';
+	$conn = new PDO( 'mysql:host=sql8.freesqldatabase.com;dbname= sql8508680', $db_username, $db_password );
 	if(!$conn){
 		die("Fatal Error: Connection Failed!");
 	}
