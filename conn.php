395bb294212d3d608fb5e81f6af6efba4872ed2d
Rabab Fahssi <fhsyrabab07@gmail.com>
2022-08-06 05:20:00 +0100

    push master

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 	$db_username = 'sql8511061';
-	$db_password = '';
+	$db_password = 'szbcX1MjwX';
 	$conn = new PDO( 'mysql:host=sql8.freesqldatabase.com;dbname=sql8511061', $db_username, $db_password );
 	if(!$conn){
 		die("Fatal Error: Connection Failed!" . mysqli_connect_error());
