01162abf7559a33ef71de1fff0aac39f55d61400
Rabab Fahssi <fhsyrabab07@gmail.com>
2022-08-23 20:18:11 +0100

    change all

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-	$db_username = 'toubkali_toubkal';
-	$db_password = '2ejCD1zp72';
-	$conn = new PDO( 'mysql:host=localhost;dbname=toubkali_medcal', $db_username, $db_password );
+	$db_username = 'root';
+	$db_password = '';
+	$conn = new PDO( 'mysql:host=localhost;dbname=gestion_cab', $db_username, $db_password );
 	if(!$conn){
 		die("Fatal Error: Connection Failed!" . mysqli_connect_error());
 	}
