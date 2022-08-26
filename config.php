1a1b773be2aa83837f59d5194b6ccfaf5017db65
Rabab Fahssi <fhsyrabab07@gmail.com>
2022-08-26 16:53:45 +0100

    last

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Informations d'identification
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'gestion_cab');
+define('DB_USERNAME', 'toubkali_toubkal');
+define('DB_PASSWORD', '2ejCD1zp72');
+define('DB_NAME', 'toubkali_medcal');
  
 // Connexion � la base de donn�es MySQL 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
